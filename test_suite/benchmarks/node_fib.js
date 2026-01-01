55a60e207ab8a0588e7b31e8d93652aba58b4c47
drkwitht <derektan3510@gmail.com>
2025-12-31 16:07:37 -0800

    Added Python 3.12 and NodeJS no-JIT benchmarks.

---
+++ test_suite/benchmarks/node_fib.js
@@ -0,0 +1,17 @@
+/**
+ * @note Use this with NodeJS V8 jitless for a fair comparison against DerkJS.
+ * @summary Does a recursive fibonacci 30 program with timings.
+ */
+
+function fib_r_node(n) {
+    if (n < 2) {
+        return n;
+    }
+
+    return fib_r_node(n - 1) + fib_r_node(n - 2);
+}
+
+const fib_begin_time_ms = Date.now();
+const ans = fib_r_node(30);
+const fib_runtime_ms = Date.now() - fib_begin_time_ms;
+console.log(`answer = ${ans}, runtime = \x1b[1;33m${fib_runtime_ms}ms\x1b[0m`);
---
+++ test_suite/benchmarks/py3_fib.py
@@ -0,0 +1,19 @@
+"""
+test_suite.benchmarks.py3_fib
+By: DrkWithT (GitHub)
+
+Contains a timed recursive Fibonacci 30 script for rough benchmarking purposes vs. DerkJS.
+"""
+
+import time
+
+def fib_py_r(n):
+    if n < 2:
+        return n
+    else:
+        return fib_py_r(n - 1) + fib_py_r(n - 2)
+
+start_fib_time_ns = time.time_ns()
+answer = fib_py_r(30)
+fib_runtime_ms = (time.time_ns() - start_fib_time_ns) / 1000000
+print(f"answer = {answer}, runtime = \x1b[1;33m{fib_runtime_ms}ms\x1b[0m")
