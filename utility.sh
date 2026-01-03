11a179263f8f49d71eea6f91421c9c0a150f94b4
drkwitht <derektan3510@gmail.com>
2026-01-02 22:31:02 -0800

    Add optional build setup support for TCO VM.

--- utility.sh
+++ utility.sh
@@ -1,7 +1,7 @@
 argc=$#
 
 usage_exit() {
-    echo "Usage: utility.sh [help | build | unittest | profile | sloc]\n\tutility.sh (re)build [debug | release]\n\tutility.sh unittest\n\tutility.sh profile <JS file path>\n";
+    echo "Usage: utility.sh [help | build | unittest | profile | sloc]\n\tutility.sh (re)build [debug | release] <has-tco>[1 | 0]\n\tutility.sh unittest\n\tutility.sh profile <JS file path>\n";
     exit $1;
 }
 
@@ -14,15 +14,15 @@ build_status=0
 
 if [[ $action = "help" ]]; then
     usage_exit 0;
-elif [[ $action = "build" && $argc -ge 2 ]]; then
+elif [[ $action = "build" && $argc -eq 3 ]]; then
     rm -rf ./.cache;
     rm -f ./build/compile_commands.json;
     rm -f ./build/derkjs;
-    cmake -S . -B build --preset "local-$2-build" && cmake --build build;
-elif [[ $action = "rebuild" && $argc -ge 2 ]]; then
+    cmake -S . -B build --preset "local-$2-build" -DUSE_TCO_WITH_BC_VM:BOOL=$3 && cmake --build build;
+elif [[ $action = "rebuild" && $argc -eq 3 ]]; then
     rm -rf ./.cache;
     rm -rf ./build/;
-    cmake --fresh -S . -B build --preset "local-$2-build" && cmake --build build;
+    cmake -S . -B build --preset "local-$2-build" -DUSE_TCO_WITH_BC_VM:BOOL=$3 && cmake --build build;
 elif [[ $action = "unittest" && $argc -eq 1 ]]; then
     # touch ./test_logs.txt;
     # ctest --test-dir build --timeout 2 -V 1> ./test_logs.txt;
