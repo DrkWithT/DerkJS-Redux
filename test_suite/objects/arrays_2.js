3ed460a01cdd3adbea072787fed16e646f2382ff
drkwitht <derektan3510@gmail.com>
2026-01-28 18:43:31 -0800

    Add basic array methods case.

---
+++ test_suite/objects/arrays_2.js
@@ -0,0 +1,16 @@
+var items = [
+    1,
+    2,
+    3,
+    true
+];
+
+var first = items.at(0);
+var last = items.pop();
+var postN = items.push(2, 1);
+
+console.log(first == 1);
+console.log(last == true);
+console.log(postN == 5);
+
+return 0;
