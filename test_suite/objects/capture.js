ec92bcdc58d01b886a996f80c73c3a4ec09233cb
drkwitht <derektan3510@gmail.com>
2026-02-08 16:46:14 -0800

    Added extra capture test case & modified node fib benchmark to match others.

--- test_suite/objects/capture.js
+++ test_suite/objects/capture.js
@@ -5,7 +5,17 @@ function test() {
     return outside;
 }
 
-// Should yield 20
+function test2() {
+    var outside = 20;
+    var test3 = function() {
+        return outside * 2 + 2;
+    };
+
+    return test3();
+}
+
+// Should yield 10 & 42
 console.log("test:", test());
+console.log("test2:", test2());
 
 return 0;
