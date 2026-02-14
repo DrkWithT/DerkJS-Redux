39cb59b45a3ddb84f3a4b11d77249479d9886b52
drkwitht <derektan3510@gmail.com>
2026-02-14 08:34:31 -0800

    Reorganize test suite cases.

--- test_suite/builtins/arrays_3.js
+++ test_suite/builtins/arrays_3.js
@@ -3,7 +3,10 @@
 var junk = [null, undefined, 69, 420];
 
 /* 2 */
-console.log(junk.indexOf(69));
+console.log(junk.indexOf(69) === 2);
+
+/* 2 */
+console.log(junk.lastIndexOf(69) === 2);
 
 /* 420, 69, undefined, null */
 console.log(junk.reverse());
---
+++ test_suite/builtins/arrays_6.js
@@ -0,0 +1,31 @@
+// Test Array.prototype.map and Array.prototype.filter:
+
+var ok = 0;
+var brainrot = [69, false, 420, null, 67, 15, true, undefined];
+var counts = [1, 2, 3];
+
+var noBrainrot = brainrot.filter(function (arg) { return typeof arg !== "number"; });
+var noBrainrotStr = noBrainrot.join();
+
+if (noBrainrotStr === "false,,true,") {
+    ok = ok + 1;
+} else {
+    console.log("Wrong brainrot.filter result:", noBrainrotStr);
+}
+
+var squaresTo3 = counts.map(function (arg) {
+    return arg * arg;
+});
+var squaresTo3Str = squaresTo3.join(); 
+
+if (squaresTo3Str === "1,4,9") {
+    ok = ok + 1;
+} else {
+    console.log("Wrong counts.map(<square fn>) result:", squaresTo3Str);
+}
+
+if (ok !== 2) {
+    return 1;
+}
+
+return 1;
